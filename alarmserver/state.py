1fbca0c12e783305264fe0ed42338a86138d3f5e
Juggie <donnyk+envisalink@gmail.com>
2016-02-16 02:49:09 -0500

    WIP

--- alarmserver/state.py
+++ alarmserver/state.py
@@ -25,7 +25,7 @@ class state():
         if zone in config.ZONENAMES:
             # save zone if not already there
             if not zone in state.state['zone']:
-                state.state['zone'][zone] = {'name' : config.ZONENAMES[zone], 'last_events' : [], 'status' : defaultStatus}
+                state.state['zone'][zone] = {'name' : config.ZONENAMES[zone], 'lastevents' : [], 'status' : defaultStatus}
         else:
             logger.debug('Ignoring unnamed zone {}'.format(zone))
 
@@ -35,13 +35,13 @@ class state():
 
     @staticmethod
     def updatePartition(code, partition, event, message, defaultStatus):
-        if not 'partition' in state.state: state.state['partition'] = {}
+        if not 'partition' in state.state: state.state['partition'] = {'lastevents' : []}
 
         # if the partition is named in the config file save info in state.state
         if partition in config.PARTITIONNAMES:
             # save partition if not already there
             if not partition in state.state['partition']:
-                state.state['partition'][partition] = {'name' : config.PARTITIONNAMES[partition], 'last_events' : [], 'status' : defaultStatus}
+                state.state['partition'][partition] = {'name' : config.PARTITIONNAMES[partition], 'lastevents' : [], 'status' : defaultStatus}
         else:
             logger.debug('Ignoring unnamed partition {}'.format(partition))
 
